84c33c14f42635bb8f14e6ed4d6f8dd85ba08b04
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-01 22:29:54 +0900

    :sparkles:feat: トップページへのリンクとログインボタンにイベントを追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   return (
-    <nav className="w-full bg-white border-b-2">
+    <div className="w-full bg-white border-b-2">
       <div className="flex justify-between items-center h-16 max-w-5xl mx-auto">
-        <div className="flex items-center bg-emerald-400 p-3">
-          <Image src="/logo.svg" alt="Logo" width={24} height={24} />
-          <h2 className="text-lg">Laterless</h2>
-        </div>
+        <nav className="bg-emerald-400 p-3">
+          <Link href="/" className="flex items-center">
+            <Image src="/logo.svg" alt="Logo" width={24} height={24} />
+            <h2 className="text-lg">Laterless</h2>
+          </Link>
+        </nav>
         <button className="bg-emerald-400 p-2 m-2 rounded-full">ログイン</button>
       </div>
-    </nav>   
+    </div>   
   )
 }
