08f86f3a599b0c895f742ec2f9924b8c6609383d
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-01 22:09:14 +0900

    :lipstick:style: ロゴとタイトルを追加

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,13 @@
+import Image from "next/image";
+
 export default function Header() {
   return (
     <nav className="w-full bg-white border-b-2">
       <div className="flex justify-between items-center h-16 max-w-5xl mx-auto">
-        <div className="bg-emerald-400 p-2">Logo</div>
+        <div className="flex items-center bg-emerald-400 p-3">
+          <Image src="/logo.svg" alt="Logo" width={24} height={24} />
+          <h2 className="text-lg">Laterless</h2>
+        </div>
         <button className="bg-emerald-400 p-2 m-2 rounded-full">ログイン</button>
       </div>
     </nav>   
