056385bb439fc8ecccf06b2a5dae1d5fa2230d77
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-02 02:13:10 +0900

    :hammer:refactor: タグ修正とデザイン調整

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,15 +2,17 @@ import Link from "next/link";
 
 export default function Footer() {
   return (
-    <div className="w-full bg-emerald-400 text-white">
-      <div className="flex justify-between items-center max-w-5xl mx-auto h-16">
-        <div>コピーライト</div>
-        <div>
+    <footer className="w-full h-16 bg-emerald-400 text-white">
+      <div className="flex justify-between items-center max-w-5xl mx-auto h-full">
+        <div className="text-sm">
+          © 2023 All rights reserved.
+        </div>
+        <nav>
           <Link href="/" className="mx-2">利用規約</Link>
           <Link href="/" className="mx-2">プライバシーポリシー</Link>
           <Link href="/" className="mx-2">お問い合わせ</Link>
-        </div>
+        </nav>
       </div>
-    </div> 
+    </footer> 
   )
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,16 +3,16 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="w-full bg-white border-b-2">
+    <header className="w-full bg-white border-b-2">
       <div className="flex justify-between items-center h-16 max-w-5xl mx-auto">
-        <nav className="bg-emerald-400 p-3">
+        <nav className="bg-emerald-400 p-3 m-2">
           <Link href="/" className="flex items-center">
             <Image src="/logo.svg" alt="Logo" width={24} height={24} />
             <h2 className="text-lg">Laterless</h2>
           </Link>
         </nav>
-        <button className="bg-emerald-400 p-2 m-2 rounded-full">ログイン</button>
+        <button className="bg-emerald-400 p-3 m-2 rounded-full">ログイン</button>
       </div>
-    </div>   
+    </header>   
   )
 }
