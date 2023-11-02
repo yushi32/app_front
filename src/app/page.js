cb84d9417241a5064707b04b9ea5ac9fe0e907b0
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-02 11:24:29 +0900

    :sparkles:feat: キャッチコピーを追加

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,22 @@
 export default function Home() {
   return (
     <>
-      <h3>コンテンツ</h3>
+      <div className="w-full bg-neutral-800 text-white">
+        <div className="flex justify-center flex-col items-center">
+          <p className="mt-6 pt-6 text-xl">
+            後で読もうと思ったまま
+          </p>
+          <p className="mb-6 text-xl">
+            放置している記事、ありませんか？
+          </p>
+          <p className="my-6 text-xl">
+            Laterlessでブックマークを効率的に管理しましょう
+          </p>
+          <button className="p-3 my-6 rounded-full bg-emerald-400">
+            今すぐ始める
+          </button>
+        </div>
+      </div>
     </>
   )
 }
