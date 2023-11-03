87025e52a9f9f558631c19b51b030ff93d6ba1f0
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-04 02:59:22 +0900

    :hammer:refactor: サインインボタンをカスタムフックを利用する形に修正

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -1,28 +1,12 @@
 'use client'
 
-import { useRouter } from "next/navigation";
-import { signInWithPopup, signInWithRedirect, GoogleAuthProvider } from "firebase/auth";
-
-import { auth } from "../lib/initFirebase";
+import useFirebaseAuth from "../hooks/useFirebaseAuth";
 
 export default function SignInButton({ text }) {
-  const router = useRouter();
-
-  const loginWithGoogle = async () => {
-    const provider = new GoogleAuthProvider();
-    const result = await signInWithPopup(auth, provider);
-  
-    if (result) {
-      const user = result.user;
-      const credential = GoogleAuthProvider.credentialFromResult(result);
-      const token = credential?.accessToken;
-      router.push("/home");
-      return user;
-    }
-  };
+  const { loginWithGoogle } = useFirebaseAuth();
 
   return (
-    <button onClick={loginWithGoogle} className="p-3 my-6 rounded-full bg-emerald-400">
+    <button onClick={loginWithGoogle} className="p-3 mx-2 my-6 rounded-full bg-emerald-400">
       {text}
     </button>
   );
