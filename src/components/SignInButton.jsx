b4e7c51584999427ac5c484ea1cd94910c4da7d2
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-05 03:55:14 +0900

    :zap:update: SignInButtonにFirebase Authenticationが発行したIDトークンをバックエンドに渡す処理を追加

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -1,12 +1,38 @@
 'use client'
 
+import axios from "axios";
+
 import useFirebaseAuth from "../hooks/useFirebaseAuth";
 
 export default function SignInButton({ text }) {
   const { loginWithGoogle } = useFirebaseAuth();
 
+  const handleGoogleLogin = () => {
+    const verifyIdToken = async () => {
+      const user = await loginWithGoogle();
+      const token = await user?.getIdToken();
+
+      const config = {
+        headers: { authorization: `Bearer ${token}` },
+      };
+
+      try {
+        axios.post('http://backend:3000/api/v1/authentication', null, config);
+      } catch (err) {
+        let message;
+        if (axios.isAxiosError(err) && err.response) {
+          console.error(err.response.data.message);
+        } else {
+          message = String(err);
+          console.error(message);
+        }
+      }
+    };
+    verifyIdToken();
+  };
+
   return (
-    <button onClick={loginWithGoogle} className="p-3 mx-2 my-6 rounded-full bg-emerald-400">
+    <button onClick={handleGoogleLogin} className="p-3 mx-2 my-6 rounded-full bg-emerald-400">
       {text}
     </button>
   );
