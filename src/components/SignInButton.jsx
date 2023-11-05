5d686dbdf6ee38889b090507824b5f72e9dfb79a
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-05 19:04:05 +0900

    :adhesive_bandage:fix: リクエスト先を修正

--- src/components/SignInButton.jsx
+++ src/components/SignInButton.jsx
@@ -17,7 +17,7 @@ export default function SignInButton({ text }) {
       };
 
       try {
-        axios.post('http://backend:3000/api/v1/authentication', null, config);
+        axios.post('http://localhost:3000/api/v1/authentication', null, config);
       } catch (err) {
         let message;
         if (axios.isAxiosError(err) && err.response) {
