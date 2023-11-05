a3b06fb0129a8bd1a5634be27c11901a2817227d
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-05 19:07:28 +0900

    :sparkles:feat: バックエンドからブックマーク一覧を取得する

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -1,12 +1,14 @@
 'use client';
 
-import { useEffect } from "react";
+import axios from "axios";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 import { useAuthContext } from "../../context/AuthContext";
 import useFirebaseAuth from "../../hooks/useFirebaseAuth";
 
 export default function Page() {
+  const [bookmarks, setBookmarks] = useState([]);
   const { currentUser, loading } = useAuthContext();
   const router = useRouter();
 
@@ -17,6 +19,24 @@ export default function Page() {
       router.push("/");
     }
   }, [currentUser, loading]);
+
+  useEffect(() => {
+    const getBookmarks = async () => {
+      const token = await currentUser?.getIdToken();
+      if (!token) {
+        return
+      } else {
+        const config = {
+          headers: { authorization: `Bearer ${token}` },
+        };
+        console.log(currentUser)
+        const res = await axios.get('http://localhost:3000/api/v1/bookmarks', config);
+        setBookmarks(res.data.data)
+        console.log(res.data.data);
+      }
+    }
+    getBookmarks();
+  }, [currentUser]);
 
   return <button onClick={logout}>ログアウト</button>;
 }
