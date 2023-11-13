89cc1a73804f46b3a324f28e8699cc0e19abc904
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-13 23:22:55 +0900

    :sparkles:feat: 削除リクエストが成功したらコンポーネントをアンマウントする

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
+import { useState } from "react"
 import Link from "next/link";
 
 import { useAuthContext } from "../context/AuthContext"
 
 export default function Card({ id, url, title }) {
+  const [isDeleted, setIsDeleted] = useState(false);
   const { currentUser } = useAuthContext();
+
   const onClickDelete = async () => {
     const token = await currentUser?.getIdToken();
       if (!token) {
@@ -15,8 +18,18 @@ export default function Card({ id, url, title }) {
         };
       const res = await axios.delete(`${process.env.NEXT_PUBLIC_API_URL}/api/v1/bookmarks/${id}`, config);
       console.log(res);
+      if (res.status === 204) {
+        setIsDeleted(true);
+        console.log('Successfully deleted');
+      } else {
+        console.log('Failed to delete');
+      }
     }
   };
+
+  if (isDeleted) {
+    return null;
+  }
 
   return (
     <div className="col-span-1 text-center rounded shadow-md bg-yellow-200 mt-4 mx-1 max-h-72 p-3">
