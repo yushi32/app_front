8a60a3143d49a3232b42b43be4fbdb42fe50d21c
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-14 17:04:15 +0900

    :lipstick:style: ログイン・ログアウトボタンに動きをつける

--- src/components/SignOutButton.jsx
+++ src/components/SignOutButton.jsx
@@ -2,7 +2,7 @@
 
 export default function SignOutButton({ logout }) {
   return (
-    <button onClick={logout} className="p-3 mx-2 my-6 rounded-full bg-emerald-400">
+    <button onClick={logout} className="p-3 mx-2 my-6 rounded-full bg-emerald-400 hover:bg-emerald-300 hover:scale-95">
       ログアウト
     </button>
   );
