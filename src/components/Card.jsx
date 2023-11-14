7b58ed99461e23e6116ab91ae7c7d65c5d8facd6
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-14 17:02:12 +0900

    :lipstick:style: Cardコンポーネントのデザインを調整

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { useState } from "react"
 import Link from "next/link";
 
+
 import { useAuthContext } from "../context/AuthContext"
 
 export default function Card({ id, url, title }) {
@@ -31,15 +32,44 @@ export default function Card({ id, url, title }) {
     return null;
   }
 
-  return (
-    <div className="col-span-1 text-center rounded shadow-md bg-yellow-200 mt-4 mx-1 max-h-72 p-3">
-      <Link href={url}>
-        {title}
-      </Link>
-      <button
-        onClick={()=> {onClickDelete(id)}}
+  const onClickEdit = () => {};
 
-      >削除</button>
+  return (
+    <div className="col-span-1 rounded-md shadow-md hover:shadow-xl mt-4 mx-1 h-64 flex flex-col justify-between border-6">
+      <Link href={url} className="flex flex-col h-full">
+      <div className={`rounded-t-md bg-neutral-800 flex-1 h-[50%]`}></div>
+
+        <div className="px-3 pt-3 pb-1 text-center overflow-scroll h-[50%] flex flex-col justify-center">
+            <span className="text-center text-sm">{title}</span>
+        </div>
+      </Link>
+      <div className=" flex justify-between px-2 py-1">
+        <div>
+          <button className="rounded-full bg-emerald-200 text-xs px-2 py-1 hover:bg-emerald-400 hover:scale-95">
+            #タグ
+          </button>
+        </div>
+        <div>
+          <button onClick={onClickEdit}>
+            <Image
+              src="/pencil.svg"
+              alt="edit"
+              width={24}
+              height={24}
+              className="transform hover:rotate-12 transition-transform duration-300"
+            />
+          </button>
+          <button onClick={onClickDelete}>
+            <Image
+              src="/delete.svg"
+              alt="delete"
+              width={24}
+              height={24}
+              className="transform hover:rotate-12 transition-transform duration-300"
+            />
+          </button>
+        </div>
+      </div>      
     </div>
-  );
+  );  
 }
