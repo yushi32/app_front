aca5c02209ad3f9c259884231353546c5f4aeb10
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-22 22:05:00 +0900

    :lipstick:style: タグの入力フォームを表示した時に要素の高さが変わらないように調整

--- src/components/AddTag.jsx
+++ src/components/AddTag.jsx
@@ -23,7 +23,7 @@ export default function AddTag() {
           </form>
         </>
       ) : (
-        <button onClick={openForm} className="rounded-full bg-emerald-200 text-xs px-2 py-1 hover:bg-emerald-400 hover:scale-95">
+        <button onClick={openForm} className="rounded-full bg-emerald-200 text-xs px-2 py-1 hover:bg-emerald-400 hover:scale-95 border border-emerald-200 hover:border-emerald-400">
           +
         </button>
       )}
--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,6 +1,6 @@
 export default function Tag({ name }) {
   return (
-    <button className="rounded-full bg-emerald-200 text-xs px-2 py-1 hover:bg-emerald-400 hover:scale-95">
+    <button className="rounded-full bg-emerald-200 text-xs px-2 py-1 hover:bg-emerald-400 hover:scale-95 border border-emerald-200 hover:border-emerald-400">
       #{name}
     </button>
   );
