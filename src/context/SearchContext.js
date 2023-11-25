e5965c435e78ba5f74aaff8f57283da5043800a6
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-25 23:08:06 +0900

    :sparkles:feat: 検索条件を保持するグローバルステートを作成

---
+++ src/context/SearchContext.js
@@ -0,0 +1,33 @@
+'use client';
+
+import { createContext, useContext, useState } from "react";
+
+const SearchContext = createContext();
+
+export function SearchProvider({ children }) {
+  const [selectedTag, setSelectedTag] = useState();
+
+  const filterByTag = (tagName) => {
+    setSelectedTag(tagName);
+  };
+
+  const removeFilter = () => {
+    setSelectedTag(null);
+  };
+
+  const handleOnClickTag = (tagName) => {
+    if (selectedTag) {
+      removeFilter();
+    } else {
+      filterByTag(tagName);
+    }
+  };
+
+  return (
+    <SearchContext.Provider value={{ selectedTag, setSelectedTag, handleOnClickTag }}>
+      {children}
+    </SearchContext.Provider>
+  );
+}
+
+export const useSearchContext = () => useContext(SearchContext);
