97465e847a2090db2444667de8434a4243d293aa
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-26 19:47:34 +0900

    :lipstick:style: どのタグで絞り込んでいるか分かりやすいように背景色を変更する

--- src/components/Tag.jsx
+++ src/components/Tag.jsx
@@ -1,7 +1,7 @@
 import { useSearchContext } from "../context/SearchContext";
 
 export default function Tag({ name }) {
-  const { handleOnClickTag } = useSearchContext();
+  const { selectedTags, handleOnClickTag } = useSearchContext();
 
   const handleOnClick = () => {
     handleOnClickTag(name);
@@ -10,7 +10,19 @@ export default function Tag({ name }) {
   return (
     <button
       onClick={handleOnClick}
-      className="rounded-full bg-emerald-200 text-xs px-2 py-1 hover:bg-emerald-400 hover:scale-95 border border-emerald-200 hover:border-emerald-400"
+      className={
+        `rounded-full
+        bg-emerald-200
+        text-xs
+        px-2
+        py-1
+        hover:bg-emerald-400
+        hover:scale-95
+        border
+        border-emerald-200
+        hover:border-emerald-400
+        ${selectedTags.includes(name) ? 'bg-emerald-400 border-emerald-400' : ''}`
+      }
     >
       #{name}
     </button>
