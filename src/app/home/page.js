93829032aa19cdc2871391d7d9fcb03f2798292a
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-28 12:20:06 +0900

    :recycle:refactor: useMemoを使ってブックマークの絞り込み処理をメモ化する

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -10,7 +10,7 @@ import Card from "../../components/Card";
 import NoContents from "../../components/NoContents";
 
 export default function Page() {
-  const { filteredBookmarks, dataLoading } = useFilteredBookmarks();
+  const { bookmarks, isLoading } = useFilteredBookmarks();
   const { currentUser, loading } = useAuthContext();
   const router = useRouter();
 
@@ -22,14 +22,14 @@ export default function Page() {
 
   return (
     <>
-      {dataLoading && !filteredBookmarks.length ? (
+      {!isLoading && bookmarks.length === 0 ? (
         <NoContents />
       ) : (
         <div className="flex-grow grid grid-cols-3 gap-x-4 gap-y-4 my-8 px-12 max-w-5xl mx-auto border-4">
-        {filteredBookmarks?.map((bookmark) => {
+        {bookmarks.map((bookmark) => {
           return (
             <Card key={bookmark.id} id={bookmark.id} url={bookmark.url} title={bookmark.title} bookmarkTags={bookmark.tags} />
-          )
+          );
         })}
         </div>
       )}
--- src/hooks/useFilteredBookmarks.js
+++ src/hooks/useFilteredBookmarks.js
@@ -1,37 +1,25 @@
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 
 import { useFetchData } from "../hooks/useFetchData";
 import { useSearchContext } from "../context/SearchContext";
 
 export function useFilteredBookmarks() {
-  const { bookmarks } = useFetchData();
-  const [filteredBookmarks, setFilteredBookmarks] = useState([]);
-  const [dataLoading, setDataLoading] = useState(false);
-  const { selectedTags, setSelectedTags } = useSearchContext();
+  const { bookmarks, error } = useFetchData();
+  const { selectedTags } = useSearchContext();
 
-  useEffect(() => {
-    if (bookmarks) {
-      setFilteredBookmarks(bookmarks);
-      setSelectedTags([]);
-      setDataLoading(true);
-    }
-  }, [bookmarks]);
-
-  useEffect(() => {
-    if (!selectedTags.length) {
-      setFilteredBookmarks(bookmarks);
-    } else {
-      const result = bookmarks.filter((bookmark) =>
-        selectedTags.every((selectedTag) =>
-          bookmark.tags.some((tag) => tag.name === selectedTag)
-        )
-      );
-      setFilteredBookmarks(result);
-    }
-  }, [selectedTags]);
+  const filteredBookmarks = useMemo(() => {
+    if (!bookmarks) return [];
+    if (selectedTags.length === 0) return bookmarks;
+    return bookmarks.filter((bookmark) =>
+      selectedTags.every((selectedTag) =>
+        bookmark.tags.some((tag) => tag.name === selectedTag)
+      )
+    );
+  }, [bookmarks, selectedTags]);
 
   return {
-    filteredBookmarks,
-    dataLoading
+    bookmarks: filteredBookmarks,
+    isLoading: !error && !bookmarks,
+    isError: error
   };
 }
