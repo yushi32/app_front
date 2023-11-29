dc01cfefb1387b1861285176ea4fe0a7c5b0dab6
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-29 18:13:24 +0900

    :lipstick:style: ヘッダーのデザインを調整

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,15 +18,15 @@ export default function Header() {
 
   return (
     <header className="sticky top-0 w-full bg-white border-b-2">
-      <div className="flex justify-between items-center h-16 max-w-5xl mx-auto">
-        <nav className="bg-emerald-400 p-3 m-2">
+      <div className="flex justify-between items-center h-16 max-w-6xl mx-auto">
+        <nav className="bg-emerald-400 p-3 my-2">
           <Link href="/" className="flex items-center">
             <Image src="/logo.svg" alt="Logo" width={24} height={24} />
             <h2 className="text-lg">Laterless</h2>
           </Link>
         </nav>
           {currentUser ? (
-            <div className="flex items-center justify-center space-x-4 px-2">
+            <div className="flex items-center justify-center space-x-4">
               <ExtensionButton textSize={'text-xs'} />
               <SignOutButton logout={logout} />
             </div>
