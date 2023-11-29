93054dd32cf73547eeac6ab94a84e0a9b294ce28
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-29 23:48:49 +0900

    :sparkles:feat: Folderコンポーネントを作成

---
+++ src/components/Folder.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+import Image from "next/image";
+
+export default function Folder({ text }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const handleOnClick = (e) => {
+    console.log(e.target.textContent);
+  };
+
+  return (
+    <div
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      className="flex items-center justify-center justify-between border-l-2 border-transparent hover:border-emerald-200"
+    >
+      <button
+        onClick={handleOnClick}
+        className="flex-1 text-left text-sm font-medium h-full py-2.5 pl-2"
+      >
+        {text}
+      </button>
+      {isHovered && (
+        <button
+          onClick={() => console.log('edit')}
+          className="pr-1"
+        >
+          <Image
+            src="/pencil.svg"
+            alt="edit"
+            width={20}
+            height={20}
+            className="hover:rotate-12 transition-transform duration-300"
+          />
+        </button>
+      )}
+    </div>
+  );
+}
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,26 @@
+import Image from "next/image";
+
+import Folder from "./Folder";
+
 export default function Sidebar() {
   return (
-    <div className="col-span-1 border-r-2 px-4">
-      <div className="bg-emerald-200 text-center">フォルダ</div>
+    <div className="col-span-1 flex flex-col border-r-2 px-4">
+      <div className="flex items-center justify-center justify-between pb-4">
+        <Image
+          src="/folder.svg"
+          alt="folder"
+          width={32}
+          height={32}
+        />
+        <button
+          className="rounded-full bg-emerald-200 text-lg px-2 hover:bg-emerald-400 hover:scale-95 border border-emerald-200 hover:border-emerald-400"
+        >
+          +
+        </button>
+      </div>
+      <Folder text={'All'} />
+      <Folder text={'フォルダ'} />
+      <Folder text={'UIコンポーネントライブラリ'} />
     </div>
   );
 }
