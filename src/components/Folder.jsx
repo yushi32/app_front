f27a0c6b42c2b9eb71f307dd23ac0e70435d6e65
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-30 22:52:27 +0900

    :sparkles:feat: Folderコンポーネントを再帰的に呼び出すことで子フォルダを表示する

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -3,37 +3,47 @@ import Image from "next/image";
 
 export default function Folder({ text, id, name, children }) {
   const [isHovered, setIsHovered] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleOnClick = (e) => {
-    console.log(e.target.textContent);
+  const openChildFolder = () => {
+    setIsOpen(prev => !prev);
   };
 
   return (
-    <div
-      onMouseEnter={() => setIsHovered(true)}
-      onMouseLeave={() => setIsHovered(false)}
-      className="flex items-center justify-center justify-between border-l-2 border-transparent hover:border-emerald-200"
-    >
-      <button
-        onClick={handleOnClick}
-        className="flex-1 text-left text-sm font-medium h-full py-2.5 pl-2"
+    <>
+      <div
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        className="flex items-center justify-center justify-between border-l-2 border-transparent hover:border-emerald-200"
       >
-        {text || name}
-      </button>
-      {isHovered && (
         <button
-          onClick={() => console.log('edit')}
-          className="pr-1"
+          onClick={openChildFolder}
+          className="flex-1 text-left text-sm font-medium h-full py-2.5 pl-2"
         >
-          <Image
-            src="/pencil.svg"
-            alt="edit"
-            width={20}
-            height={20}
-            className="hover:rotate-12 transition-transform duration-300"
-          />
+          {text || name}
         </button>
-      )}
-    </div>
+        {isHovered && (
+          <button
+            onClick={() => console.log('edit')}
+            className="pr-1"
+          >
+            <Image
+              src="/pencil.svg"
+              alt="edit"
+              width={20}
+              height={20}
+              className="hover:rotate-12 transition-transform duration-300"
+            />
+          </button>
+        )}
+      </div>
+      {isOpen && children && children?.map((folder) => {
+        return (
+          <div key={folder.id} className="pl-4">
+            <Folder id={folder.id} name={folder.name} children={folder.children} />
+          </div>  
+          );
+      })}
+    </>
   );
 }
