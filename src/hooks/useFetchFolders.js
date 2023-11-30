ee5ca00733a52451bff022a3f9431bd916b6a475
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-11-30 21:49:08 +0900

    :sparkles:feat: バックエンドからフォルダ一覧を取得するカスタムフックを作成

---
+++ src/hooks/useFetchFolders.js
@@ -0,0 +1,39 @@
+import axios from "axios";
+import { useEffect } from "react";
+import useSWR from 'swr';
+
+import { useAuthContext } from "../context/AuthContext";
+
+const fetcher = async (url, currentUser) => {
+  return currentUser?.getIdToken()
+    .then((token) => {
+      if (!token) {
+        throw new Error('No token found');
+      }
+      const config = {
+        headers: { authorization: `Bearer ${token}` },
+      };
+      return axios.get(`${process.env.NEXT_PUBLIC_API_URL}${url}`, config);
+    })
+    .then((res) => {
+      return res.data.folders;
+    });
+};
+
+export function useFetchFolders() {
+  const { currentUser } = useAuthContext();
+  const { data: folders, error } = useSWR(
+    currentUser ? [`/api/v1/folders`, currentUser] : null, 
+    ([url, currentUser]) => fetcher(url, currentUser)
+  );
+
+  useEffect(() => {
+    if (error) console.log(`error message: ${error}`);
+  }, [error]);
+
+  return {
+    folders,
+    isLoading: !folders && !error,
+    error
+  };
+}
