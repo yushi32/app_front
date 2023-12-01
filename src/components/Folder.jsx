0201f798f3f16858d5a6864cd4685ee2ea61cc79
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-12-01 23:59:46 +0900

    :sparkles:feat: 編集ボタンを押すと入力フォームが表示される

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -1,12 +1,37 @@
 import { useState } from "react";
 import Image from "next/image";
 
+import { useToggleForm } from "../hooks/useToggleForm";
+
 export default function Folder({ text, id, name, children }) {
   const [isHovered, setIsHovered] = useState(false);
   const [isOpen, setIsOpen] = useState(false);
+  const {
+    form,
+    input,
+    isFocused,
+    formRef,
+    setForm,
+    setInput,
+    openForm,
+    closeForm,
+    handleFocus,
+    handleBlur
+  } = useToggleForm();
 
   const openChildFolder = () => {
     setIsOpen(prev => !prev);
+  };
+
+  const handleOnSubmit = async (e) => {
+    e.preventDefault();
+    console.log(input);
+    setInput('');
+    setForm(false)
+  };
+
+  const handleInputChange = (e) => {
+    setInput(e.target.value);
   };
 
   return (
@@ -14,17 +39,43 @@ export default function Folder({ text, id, name, children }) {
       <div
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
-        className="flex items-center justify-center justify-between border-l-2 border-transparent hover:border-emerald-200"
+        className={`flex items-center justify-center ${!form && 'justify-between'} border-l-2 border-transparent hover:border-emerald-200`}
       >
-        <button
-          onClick={openChildFolder}
-          className="flex-1 text-left text-sm font-medium h-full py-2.5 pl-2"
-        >
-          {text || name}
-        </button>
-        {isHovered && (
+        {form ? (
+          <>
+          <form
+            onSubmit={handleOnSubmit}
+            ref={formRef}
+            className={`flex items-center my-1.5 ml-2 w-full rounded-full border ${isFocused ? "border-blue-400" : ""}`}
+          >
+            <input
+              type="text"
+              value={input}
+              onChange={handleInputChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+              className={"focus:outline-none rounded-full border border-transparent text-sm p-0.5 pl-2"}
+            />
+            <button
+              type="button"
+              onClick={closeForm}
+              className={`rounded-full bg-emerald-200 hover:bg-emerald-400 hover:scale-95 text-sm w-6 h-6 ml-4`}
+            >
+              -
+            </button>
+          </form>
+        </>
+        ) : (
           <button
-            onClick={() => console.log('edit')}
+            onClick={openChildFolder}
+            className="flex-1 text-left text-sm font-medium h-full py-2.5 pl-2"
+          >
+            {text || name}
+          </button>
+        )}
+        {!form && isHovered && (
+          <button
+            onClick={openForm}
             className="pr-1"
           >
             <Image
