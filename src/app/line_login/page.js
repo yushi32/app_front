a1c806cd2e8baa9f7e0f8d0011507a4087caf359
yushi32 <133078588+yushi32@users.noreply.github.com>
2023-12-23 19:58:27 +0900

    :zap:update: LINEアカウント連携ページに手順を記載

--- src/app/line_login/page.js
+++ src/app/line_login/page.js
@@ -17,28 +17,88 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="flex-grow flex flex-col items-center max-w-7xl mx-auto">
-      <div>
-        <div className="my-12 text-xl">LINEアカウント連携</div>
-      </div>
-      <div className="flex flex-col items-center space-y-4">
-        <div className="text-center">
-          <div>
-            LINEアカウントと連携することで未読のブックマークをLINEに通知することが出来ます。
-          </div>
-          <div>
-            下のボタンを押して、LINEにログインしてください。
+    <div className="flex-grow flex flex-col max-w-5xl mx-auto pt-12 pb-16 text-neutral-700 bg-red-20">
+      <div className="mb-8">
+        <div>
+          <div className="text-2xl font-semibold text-center">LINEアカウント連携</div>
+        </div>
+        <div className="flex flex-col text-center">
+          <div className="py-4 bg-blue-2">
+            <div>
+              LINEアカウントと連携することで未読のブックマークをLINEに通知することが出来ます。
+            </div>
+            <div>
+              下記の手順に従って、LINEアカウントを連携してください。
+            </div>
           </div>
         </div>
-        <Link
-          href={`https://access.line.me/oauth2/v2.1/authorize?${query}`}
-        >
-          <button
-            className="rounded-md p-2 bg-emerald-400 hover:bg-emerald-200 hover:scale-95"
+      </div>
+
+      <div className="space-y-12">
+        <div className="flex flex-col items-center">
+          <h2 className="w-full text-center text-xl font-bold border-b-2 p-2">手順1</h2>
+          <div className="mt-4 mb-2">
+            <div>下のボタンを押して、LINEにログインしてください。</div>
+            <div>LINEのログインページにリダイレクトされます。</div>
+          </div>
+          <Link
+            href={`https://access.line.me/oauth2/v2.1/authorize?${query}`}
           >
-            LINEにログインする
+            <button
+              className="rounded-md p-2 bg-emerald-400 text-black hover:bg-emerald-200 hover:scale-95"
+            >
+              LINEにログインする
+            </button>
+          </Link>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="w-full text-center text-xl font-bold border-b-2 p-2">手順2</h2>
+          <div className="mt-4 mb-2">
+            <div>ログイン後、同意画面が表示されるので下部にある「許可する」を押してください。</div>
+            <div>「許可する」を押した後、またこちらのページにリダイレクトされます。</div>
+          </div>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="w-full text-center text-xl font-bold border-b-2 p-2">手順3</h2>
+          <div className="mt-4 mb-2">
+            <div>こちらのページに戻ってきたら、下の「連携する」ボタンを押してください。</div>
+          </div>
+          <button
+            className="rounded-md p-2 bg-emerald-400 text-black hover:bg-emerald-200 hover:scale-95"
+          >
+            連携する
           </button>
-        </Link>
+        </div>
+
+        <div className="flex flex-col items-center">
+          <h2 className="w-full text-center text-xl font-bold border-b-2 p-2">手順4</h2>
+          <div className="mt-4 mb-2 space-y-4">
+            <div className="flex flex-col items-center">
+              <div>LINEとの連携は以上で完了です。</div>
+            </div>
+            <div className="flex flex-col items-center">
+              <div>
+                <div>通知を受け取るためには、laterless公式アカウントの友だち追加が必要です。</div>
+                <div>まだお済みでない方はこちらのQRコードから友だち追加してください。</div>
+              </div>
+              <div className="w-36 h-36 bg-red-200"></div>
+            </div>
+            <div className="flex flex-col items-center space-y-2">
+              <div>未読のブックマークの通知機能をONにしたい方は下のボタンを押してください。</div>
+              <button
+                className="rounded-md p-2 bg-emerald-400 text-black hover:bg-emerald-200 hover:scale-95"
+              >
+                通知をONにする
+              </button>
+            </div>
+            <div className="flex flex-col items-center">
+              <div className="text-sm">＊現在は毎週土曜日の夜8時に通知されるようになっています。</div>
+              <div className="text-sm">今後、通知日時を変更する機能を追加予定です。しばらくお待ちください。</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
