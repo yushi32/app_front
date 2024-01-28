acdcec6fc5b9b7626cda25e9ed0b91d47e8a4d3e
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-01-28 22:33:02 +0900

    :heavy_plus_sign:chore: UIコンポーネントライブラリ 'PrimeReact'のインストールと導入

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from 'next/font/google'
 import './globals.css'
+import "primereact/resources/themes/lara-light-cyan/theme.css";
+import { PrimeReactProvider } from 'primereact/api';
 
 import Header from "../components/Header";
 import { AuthContextProvider } from "../context/AuthContext";
@@ -14,15 +16,17 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <AuthContextProvider>
-      <SearchContextProvider>
-        <html lang="ja">
-          <body className={`${inter.className} flex flex-col min-h-screen`}>
-            <Header />
-            {children}
-          </body>
-        </html>
-      </SearchContextProvider>
-    </AuthContextProvider>
+    <PrimeReactProvider>
+      <AuthContextProvider>
+        <SearchContextProvider>
+          <html lang="ja">
+            <body className={`${inter.className} flex flex-col min-h-screen`}>
+              <Header />
+              {children}
+            </body>
+          </html>
+        </SearchContextProvider>
+      </AuthContextProvider>
+    </PrimeReactProvider>
   )
 }
