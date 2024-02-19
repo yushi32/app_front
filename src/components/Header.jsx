29ed037264f8a62b0afcc355c17bd5f223858069
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-02-19 23:40:57 +0900

    :lipstick:style: ロゴを変更

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -17,10 +17,9 @@ export default function Header() {
   return (
     <header className="sticky top-0 w-full bg-white border-b-2">
       <div className="flex justify-between items-center h-16 max-w-6xl mx-auto">
-        <nav className="bg-emerald-400 p-3 my-2">
+        <nav className="p-3 my-2">
           <Link href="/home" className="flex items-center">
-            <Image src="/logo.svg" alt="Logo" width={24} height={24} />
-            <h2 className="text-lg">Laterless</h2>
+            <Image src="/logo.svg" alt="Logo" width={150} height={45} />
           </Link>
         </nav>
         {currentUser && <SearchForm />}
