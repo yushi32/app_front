df1d3fa07ba74782a39dffb3cd810fb89e94c084
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-03-01 12:21:50 +0900

    :sparkles: feat: Chrome拡張機能へのリンクを別タブで開く

--- src/components/ExtensionButton.jsx
+++ src/components/ExtensionButton.jsx
@@ -4,6 +4,8 @@ export default function ExtensionButton({ textSize='text-sm' }) {
   return (
     <Link
       href="https://chromewebstore.google.com/detail/laterless-extension/lhickbhpcfdhpljclokijligngkcdmij?hl=ja"
+      target="_blank"
+      rel="noopener noreferrer"
       className={`rounded-full text-center ${textSize} bg-emerald-400 p-2 hover:bg-emerald-200 hover:scale-95`}
     >
       <p>Chrome拡張機能を</p>
