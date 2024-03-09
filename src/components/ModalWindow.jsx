b5910616be066c3c3fbb7ab0717517f4423fe922
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-03-10 01:10:44 +0900

    :sparkles: feat: モーダルのマスク部分をクリックした時にモーダルを閉じる

--- src/components/ModalWindow.jsx
+++ src/components/ModalWindow.jsx
@@ -1,8 +1,22 @@
+import { useEffect } from "react";
 import { Dialog } from "primereact/dialog";
 
 import BookmarkDetails from "./BookmarkDetails";
         
 export default function ModalWindow({ isModalOpen, setIsModalOpen, selectedBookmark }) {
+  useEffect(() => {
+    const handleClickMask = (event) => {
+      if (event.target.classList.contains('p-dialog-mask')) {
+        setIsModalOpen(false);
+      }
+    };
+
+    document.addEventListener('click', handleClickMask);
+    return () => {
+      document.removeEventListener('click', handleClickMask);
+    };
+  }, [setIsModalOpen]);
+
   return (
     <Dialog
       visible={isModalOpen}
