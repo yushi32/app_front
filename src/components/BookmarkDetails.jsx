b3defd4b9959c9347bf2ce3a4adf11d97aea74fc
yushi32 <133078588+yushi32@users.noreply.github.com>
2024-03-13 22:17:52 +0900

    :construction: wip: ブックマーク詳細モーダルの雛形を作成

--- src/components/BookmarkDetails.jsx
+++ src/components/BookmarkDetails.jsx
@@ -1,17 +1,46 @@
 export default function BookmarkDetails({ id, title, url, thumbnail, tags, folder_id }) {
 
   return (
-    <div className="w-[600px] h-[600px] px-12 pt-6 bg-red-200">
-      <p>ブックマーク詳細モーダルだよ</p>
-      <p>id: {id}</p>
-      <p>title: {title}</p>
-      <p>url: {url}</p>
-      <p>tag:
-        {tags.length > 0 &&
-          tags.map((tag) => <span>{tag.name} </span>)
-        }
-      </p>
-      <p>folder_id: {folder_id}</p>
+    <div className="flex flex-col w-[600px] min-h-[600px] px-12">
+      <form className="mt-6 space-y-4">
+        <div className="flex flex-col space-y-1">
+          <label htmlFor="title">タイトル</label>
+          <input
+            id="title"
+            type="text"
+            value={title}
+            className="rounded-md px-2 py-1 border"
+          />
+        </div>
+        <div className="flex flex-col space-y-1">
+          <label htmlFor="note">ノート</label>
+          <textarea
+            id="note"
+            className="rounded-md px-2 py-1 border"
+          />
+        </div>
+        <div className="flex flex-col space-y-1">
+          <label htmlFor="folder">フォルダ</label>
+          <input
+            id="folder"
+            className="rounded-md px-2 py-1 border"
+          />
+        </div>
+        <div className="flex flex-col space-y-1">
+          <label htmlFor="tags">タグ</label>
+          <input
+            id="tags"
+            className="rounded-md px-2 py-1 border"
+          >
+          </input>
+        </div>
+        <button
+          type="submit"
+          className="float-right rounded-md px-2 py-1 bg-emerald-200 hover:bg-emerald-400"
+        >
+          更新
+        </button>
+      </form>
     </div>
   );
 }
